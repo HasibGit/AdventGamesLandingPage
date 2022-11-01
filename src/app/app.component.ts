21df9836f56169c15c2c6b290a015631bb032a49
Hasib Ullah <hasibullah04@gmail.com>
2022-11-01 15:44:37 +0600

    countdown timer text format modification done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountdownConfig, CountdownEvent } from 'ngx-countdown/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'space-shooter-landing-page';
   secondsLeft: number = 0;
+  config: CountdownConfig;
 
   ngOnInit() {
     this.getSecondsLeftTillNextOffer();
@@ -19,5 +21,29 @@ export class AppComponent implements OnInit {
     tomorrow.setDate(tomorrow.getDate() + 1);
     tomorrow.setHours(0, 0, 0, 0);
     this.secondsLeft = (tomorrow.getTime() - today.getTime()) / 1000;
+    this.config = {
+      leftTime: this.secondsLeft,
+      format: 'H m s',
+      prettyText: (text) => this.parseCountdownText(text),
+    };
+  }
+
+  handleEvent(event: CountdownEvent) {}
+
+  parseCountdownText(text: string) {
+    // 1. Seperate string between spaces
+    const splittedText = text.split(/(\s+)/);
+
+    // 2. Add labels to each string array
+    const hours =
+      '<span class="winning-criteria">' + splittedText[0] + '</span> hours : ';
+    const minutes =
+      '<span class="winning-criteria">' +
+      splittedText[2] +
+      '</span> minutes : ';
+    const seconds =
+      '<span class="winning-criteria">' + splittedText[4] + '</span> seconds ';
+
+    return hours + minutes + seconds;
   }
 }
