099914c9bab429ebacb413fb8beda545dd2a1a31
Hasib Ullah <hasibullah04@gmail.com>
2022-11-02 23:18:08 +0600

    Emil landing page design done

---
+++ src/Emil_Landing_Page_Config/emil-config.ts
@@ -0,0 +1,5 @@
+export const emilConfig = {
+  brandLogoPath: '../assets/images/company-logo.png',
+  emilOfferDetails:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque molestias fugiatquod error beatae laudantium totam soluta impedit, accusamus eligendi nostrum. Modi, delectus obcaecati in commodidignissimos voluptates at deserunt eligendi quia exercitationem beatae ullam consequuntur perspiciatis laboriosam molestiae facilis repellendus sed esse nemo id. Hic commodi quaerat veritatis error?',
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 import { CountdownConfig, CountdownEvent } from 'ngx-countdown/interfaces';
 import { environment } from 'src/environments/environment';
 import { spigaConfig } from '../Spiga_Landing_Page_Config/spiga-config';
+import { emilConfig } from '../Emil_Landing_Page_Config/emil-config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'space-shooter-landing-page';
   secondsLeft: number = 0;
   config: CountdownConfig;
@@ -17,6 +24,8 @@ export class AppComponent implements OnInit {
   brandLogoPath: string;
   offerDetails: string;
 
+  constructor(private renderer: Renderer2, private elemRef: ElementRef) {}
+
   ngOnInit() {
     this.currentEnvironment = environment.environmentName;
     this.setEnvironmentSpecificConfigs();
@@ -24,10 +33,60 @@ export class AppComponent implements OnInit {
     this.getSecondsLeftTillNextOffer();
   }
 
+  ngAfterViewInit(): void {
+    this.setBubblesBackgroundColor();
+  }
+
+  setBubblesBackgroundColor() {
+    if (
+      this.currentEnvironment == 'DEV' ||
+      this.currentEnvironment == 'SPIGA'
+    ) {
+      let typeOneBubbles =
+        this.elemRef.nativeElement.querySelectorAll('.bubble-type-1');
+      [...typeOneBubbles].forEach((element) => {
+        this.renderer.setStyle(element, 'backgroundColor', '#eee4e5');
+      });
+
+      let typeTwoBubbles =
+        this.elemRef.nativeElement.querySelectorAll('.bubble-type-2');
+      [...typeTwoBubbles].forEach((element) => {
+        this.renderer.setStyle(element, 'backgroundColor', '#caaaaf');
+      });
+
+      let typeThreeBubbles =
+        this.elemRef.nativeElement.querySelectorAll('.bubble-type-3');
+      [...typeThreeBubbles].forEach((element) => {
+        this.renderer.setStyle(element, 'backgroundColor', '#863a46');
+      });
+    } else if (this.currentEnvironment == 'EMIL') {
+      let typeOneBubbles =
+        this.elemRef.nativeElement.querySelectorAll('.bubble-type-1');
+      [...typeOneBubbles].forEach((element) => {
+        this.renderer.setStyle(element, 'backgroundColor', '#e9e9e9');
+      });
+
+      let typeTwoBubbles =
+        this.elemRef.nativeElement.querySelectorAll('.bubble-type-2');
+      [...typeTwoBubbles].forEach((element) => {
+        this.renderer.setStyle(element, 'backgroundColor', '#babab8');
+      });
+
+      let typeThreeBubbles =
+        this.elemRef.nativeElement.querySelectorAll('.bubble-type-3');
+      [...typeThreeBubbles].forEach((element) => {
+        this.renderer.setStyle(element, 'backgroundColor', '#71716f');
+      });
+    }
+  }
+
   setEnvironmentSpecificConfigs() {
     if (this.currentEnvironment == 'SPIGA') {
       this.brandLogoPath = spigaConfig.brandLogoPath;
       this.offerDetails = spigaConfig.spigaOfferDetails;
+    } else {
+      this.brandLogoPath = emilConfig.brandLogoPath;
+      this.offerDetails = emilConfig.emilOfferDetails;
     }
   }
 
