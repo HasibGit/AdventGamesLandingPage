e89840d486e7e10bf65e83ca40baf8887d1008a8
Hasib Ullah <hasibullah04@gmail.com>
2022-11-06 18:01:35 +0600

    emil brand logo and font added. need to further fix responsiveness

--- src/Landing_Page_Configs/Emil_Landing_Page_Config/emil-config.ts
+++ src/Landing_Page_Configs/Emil_Landing_Page_Config/emil-config.ts
@@ -1,5 +1,5 @@
 export const emilConfig = {
-  brandLogoPath: '../assets/images/spiga_brand_logo.png',
+  brandLogoPath: '../assets/images/emil_brand_logo.png',
   emilOfferDetails:
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque molestias fugiatquod error beatae laudantium totam soluta impedit, accusamus eligendi nostrum. Modi, delectus obcaecati in commodidignissimos voluptates at deserunt eligendi quia exercitationem beatae ullam consequuntur perspiciatis laboriosam molestiae facilis repellendus sed esse nemo id. Hic commodi quaerat veritatis error?',
 };
