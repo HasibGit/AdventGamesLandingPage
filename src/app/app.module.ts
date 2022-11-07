5f67d1af24b538f675e4e298823362d655ebbf3b
Hasib Ullah <hasibullah04@gmail.com>
2022-11-07 15:28:26 +0600

    lang change button initial design done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatChipsModule } from '@angular/material/chips';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountdownModule } from 'ngx-countdown';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, CountdownModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    CountdownModule,
+    MatChipsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
